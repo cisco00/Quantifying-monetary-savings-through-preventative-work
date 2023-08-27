9e1f868daaece5f9b8983ab5c203929096d63b77
cisco00 <idokofrancis66@gmail.com>
2023-08-27 11:21:22 +0100

    update file

--- pages/About_Model.py
+++ pages/About_Model.py
@@ -11,5 +11,6 @@ st.sidebar.success("Select a demo above.")
 st.markdown("""
 A linear regression model was created to forecast the cost of both social workers and community connections as part 
             of the investigation. Linear Regression Measures the association between two or more variables. 
-            A dependent variable is predicted using this modeling approach based on one or more independent factors.. 
+            A dependent variable is predicted using this modeling approach based on one or more independent factors..
+            **👈 Select a model type and care service prediction from the sidebar** 
     """)
