fc37b1ec96a343351f86ae114485e4b298adc541
cisco00 <idokofrancis66@gmail.com>
2023-08-29 11:05:09 +0100

    updated_app_model_file

--- pages/About_Model.py
+++ pages/About_Model.py
@@ -13,5 +13,6 @@ st.markdown("""
             and community connections as part of the investigation. This will help to predict 
             the value of preventive measures and how quantitative monetary estimation can be 
             used to make more informed decisions..
-            **👈 Select a model type and care service prediction from the sidebar** 
+            **👈 Select a model type and care service prediction from the sidebar**
+            Though their is still need for improving the model because of the size of the dataset.
     """)
